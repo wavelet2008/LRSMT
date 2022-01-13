20888a1f9ff6f8562fc168b24de3cd3191e88df6
Ryoma_Liu <shipengl@usc.edu>
2022-01-13 10:22:03 -0800

    remove build things

--- agiletaur/src/build/odrive_ros2_control/colcon_command_prefix_build.sh
+++
@@ -1,4 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/shipengl/LRSMT/agiletaur/src/install/odrive_description/share/odrive_description/package.sh"
-. "/home/shipengl/LRSMT/agiletaur/src/install/odrive_hardware_interface/share/odrive_hardware_interface/package.sh"
-. "/home/shipengl/LRSMT/agiletaur/src/install/odrive_bringup/share/odrive_bringup/package.sh"
--- agiletaur/src/build/webots_simulation_assets/CMakeFiles/webots_simulation_assets_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/webots_simulation_assets_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/webots_simulation_assets_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
